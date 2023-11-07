fabbdf34f897c6d0f89882f4c6da2e47b8acb523
Drew Sirenko <68304519+AndrewSirenko@users.noreply.github.com>
2023-11-06 22:18:27 -0500

    Add initial fishshell setup

---
+++ fish/config.fish
@@ -0,0 +1,38 @@
+if status is-interactive
+    ### Modes
+    fish_vi_key_bindings
+
+    ### Environment Variables
+
+    ### Path
+    fish_add_path /opt/homebrew/bin/
+
+    ### Abbreviations
+    # Better defaults
+    abbr sed gsed
+
+    # git (TODO move most to .gitconfig)
+    abbr g git
+    abbr gs git status
+    abbr ga git add
+    abbr gc git commit
+    abbr gca git commit --amend --no-edit
+    abbr gd git diff
+    abbr gf git fetch
+    abbr gl git log
+
+    # K8s
+    abbr kgp kubectl get pods --all-namespaces
+    abbr kc kubectl
+    abbr kns kubens
+
+    ### Function abbreviations
+    # .... -> cd ../../..
+    abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+    # bash's !!
+    abbr -a !! --position anywhere --function last_history_item
+
+    # Better cd using fzf (adapted from nixcast)
+    abbr f cd_with_fzf
+end
---
+++ fish/functions/cd_with_fzf.fish
@@ -0,0 +1,3 @@
+function cd_with_fzf
+    cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
+end
