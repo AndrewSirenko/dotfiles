87b8adab218c5aa0c7e69dc13bac00ebaa9be398
Drew Sirenko <68304519+AndrewSirenko@users.noreply.github.com>
2023-12-29 19:43:34 -0500

    Add zsh abbreviations to abbr.zsh

---
+++ abbr.fish
@@ -0,0 +1,33 @@
+abbr -a -- km 'mwinit -s -o'
+abbr -a -- cdd 'ssh andsirey-clouddesk.aka.corp.amazon.com'
+abbr -a --position anywhere -- bep '--endpoint https://api.beta.us-west-2.wesley.amazonaws.com'
+abbr -a -- sed gsed
+abbr -a -- cat 'bat -p'
+abbr -a -- v nvim
+abbr -a -- g git
+abbr -a -- gs 'git status -sb'
+abbr -a -- ga 'git add'
+abbr -a -- gc 'git commit'
+abbr -a -- gca 'git commit --amend --only'
+abbr -a -- gcan 'git commit --amend --no-edit'
+abbr -a -- gd batdiff
+abbr -a -- gf 'git fetch'
+abbr -a -- gl 'git log'
+abbr -a -- kg 'kubectl get'
+abbr -a -- kgp 'kubectl get pods'
+abbr -a -- kgpa 'kubectl get pods -A'
+abbr -a -- kn kubens
+abbr -a -- kd 'kubectl describe'
+abbr -a -- kl 'kubectl logs -f'
+abbr -a -- k kubectl
+abbr -a -- ka 'kubectl apply -f'
+abbr -a -- kex 'kubectl exec -it'
+abbr -a -- ks 'kubectl --namespace kube-system'
+abbr -a -- ksl 'kubectl --namespace kube-system logs -f'
+abbr -a -- ksf 'kubectl --namespace kube-system describe'
+abbr -a -- fv vim_with_fzf
+abbr -a -- fc cd_with_fzf
+abbr -a -- cdr 'cd /Volumes/workplace/dotfiles'
+abbr -a -- root 'cd /Volumes/workplace/dotfiles'
+abbr -a --regex ^\\.\\.+\$ --function multicd -- dotdot
+abbr -a --position anywhere --function last_history_item -- !!
